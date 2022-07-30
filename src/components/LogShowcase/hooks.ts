bc29fde33ef1c23f30fe398ed879781b75b3eb83
Ignas <ignas.jasonas@telesoftas.com>
2022-07-30 21:15:57 +0300

    Fix initial animation issue

--- src/components/LogShowcase/hooks.ts
+++ src/components/LogShowcase/hooks.ts
@@ -8,30 +8,39 @@ interface CardLog extends LogExtended {
 
 export const useAnimatedCards = (log?: LogExtended) => {
   const [newLog, setNewLog] = useState<LogExtended>();
+  const [skipAnimation, setSkipAnimation] = useState(true);
   const [cards, setCards] = useState<Array<Partial<CardLog>>>([
     { key: generateId(7) },
-    { key: generateId(7) },
+    { key: generateId(7), ...log },
     { key: generateId(7) },
   ]);
 
   useEffect(() => {
     if (log) {
       let tempCards = [...cards];
-      const shouldShiftLeft = log.id >= (newLog?.id || 0);
       setNewLog(log);
-      tempCards[0] = { ...tempCards[0], ...log };
-      tempCards[2] = { ...tempCards[2], ...log };
-      if (shouldShiftLeft) {
-        tempCards.shift();
-        tempCards[2] = { key: generateId(7) };
-      } else {
-        tempCards[2] = tempCards[1];
-        tempCards[1] = tempCards[0];
-        tempCards[0] = { key: generateId(7) };
+      if (skipAnimation) {
+        setSkipAnimation(false);
+        tempCards[1] = { ...tempCards[1], ...log };
       }
-      console.log(tempCards);
+      if (!skipAnimation) {
+        const shouldShiftLeft = log.id >= (newLog?.id || 0);
+        const shouldShiftRight = !shouldShiftLeft;
+        tempCards[0] = { ...tempCards[0], ...log };
+        tempCards[2] = { ...tempCards[2], ...log };
+        if (shouldShiftLeft) {
+          tempCards.shift();
+          tempCards[2] = { key: generateId(7) };
+        }
+        if (shouldShiftRight) {
+          tempCards[2] = tempCards[1];
+          tempCards[1] = tempCards[0];
+          tempCards[0] = { key: generateId(7) };
+        }
+      }
       setCards(tempCards);
     }
+
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [log]);
 
