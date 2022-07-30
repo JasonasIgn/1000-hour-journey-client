21e099002d77e70fb4ff63c07a6e5d781a556685
Ignas <ignas.jasonas@telesoftas.com>
2022-07-30 10:32:46 +0300

    Add slider log displaying

--- src/components/JourneyTimeLine/JourneyTimeLine.tsx
+++ src/components/JourneyTimeLine/JourneyTimeLine.tsx
@@ -1,11 +1,37 @@
-import { Box } from "@chakra-ui/react";
-import { FC } from "react";
+import { Box, Slider, SliderThumb, SliderTrack } from "@chakra-ui/react";
+import { FC, useState } from "react";
 import { Journey } from "../../store/features/journeys/types";
 
 interface JourneyTimeLineProps {
   journey: Journey;
 }
 
-export const JourneyTimeLine: FC<JourneyTimeLineProps> = () => {
-  return <Box width="100%" height={1} bg="red" />;
+export const JourneyTimeLine: FC<JourneyTimeLineProps> = ({ journey }) => {
+  const [value, setValue] = useState(journey.totalHours);
+  return (
+    <Slider
+      defaultValue={value}
+      min={0}
+      max={1000}
+      colorScheme="teal"
+      onChange={(v) => setValue(v)}
+    >
+      <SliderTrack display="flex">
+        {journey.logs.map((log) => {
+          const widthPercentage = log.hoursSpent / 10;
+          return (
+            <Box
+              bg="red"
+              width={`${widthPercentage}%`}
+              boxShadow="-1px 0px 0 black"
+              _hover={{ bg: "blue" }}
+            />
+          );
+        })}
+        {/* <SliderFilledTrack /> */}
+      </SliderTrack>
+
+      <SliderThumb />
+    </Slider>
+  );
 };
