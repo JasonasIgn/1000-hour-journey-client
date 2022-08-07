50081faa38bde16d12c4eda8bebea328608eb876
Ignas <ignas.jasonas@telesoftas.com>
2022-08-07 15:36:00 +0300

    Add ruler

--- src/components/TimelineRuler/TimelineRuler.tsx
+++ src/components/TimelineRuler/TimelineRuler.tsx
@@ -1,4 +1,4 @@
-import { SliderMark } from "@chakra-ui/react";
+import { Box, SliderMark, Text } from "@chakra-ui/react";
 import React, { FC } from "react";
 
 interface TimelineRulerProps {
@@ -12,15 +12,26 @@ export const TimelineRuler: FC<TimelineRulerProps> = React.memo(
       <>
         {Array.from(Array(1000).keys()).map((val) => {
           if (Math.abs(Math.abs(currentViewX / 9.5) + 20 - val) <= 30) {
+            const shouldDisplayNumber = val % 10 === 0;
             return (
               <SliderMark
                 key={val}
                 value={val}
-                textAlign="center"
                 color="white"
-                fontSize="10px"
+                display="flex"
+                justifyContent="center"
               >
-                {val % 10 === 0 && <>{val}</>}
+                <Box
+                  width="1px"
+                  height={shouldDisplayNumber ? "6px" : "3px"}
+                  bgColor="white"
+                  mt="3px"
+                />
+                {shouldDisplayNumber && (
+                  <Text fontSize="8px" position="absolute" top="9px">
+                    {val}
+                  </Text>
+                )}
               </SliderMark>
             );
           }
