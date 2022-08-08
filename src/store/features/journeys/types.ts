d1ff50fda0e063f04360085827049f4910f590a9
Ignas <ignas.jasonas@telesoftas.com>
2022-08-08 22:29:00 +0300

    Add Achievement type

--- src/store/features/journeys/types.ts
+++ src/store/features/journeys/types.ts
@@ -23,6 +23,18 @@ export interface LogExtended extends Log {
   number: number;
 }
 
+export interface Achievement {
+  id: number;
+  journeyId: number;
+  loggedAtHour: number;
+  description: string;
+  loggedOnDate: Date;
+  createdAt: Date;
+  updatedAt: Date;
+  mediaUrl?: string;
+}
+
 export interface Journey extends JourneyListItem {
   logs: Log[];
+  achievements: Achievement[];
 }
