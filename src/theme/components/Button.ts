3c13f0d04beac821c346d5a7aba045527c9bc7e5
Ignas <ignas.jasonas@telesoftas.com>
2022-08-20 23:40:38 +0300

    Update ghost button type

--- src/theme/components/Button.ts
+++ src/theme/components/Button.ts
@@ -1,6 +1,14 @@
 import { ComponentStyleConfig } from "@chakra-ui/react";
 
 export const ButtonStyle: ComponentStyleConfig = {
+  variants: {
+    ghost: {
+      color: "brand.100",
+      _hover: {
+        color: "brand.600",
+      },
+    },
+  },
   defaultProps: {
     colorScheme: "brand",
   },
