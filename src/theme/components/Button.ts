3a7c6db6b12abdc64dbc3949e6bb8c45e3bba7b5
Ignas <ignas.jasonas@telesoftas.com>
2022-08-20 23:43:15 +0300

    Update solid button color

--- src/theme/components/Button.ts
+++ src/theme/components/Button.ts
@@ -2,6 +2,9 @@ import { ComponentStyleConfig } from "@chakra-ui/react";
 
 export const ButtonStyle: ComponentStyleConfig = {
   variants: {
+    solid: {
+      color: "gray.300",
+    },
     ghost: {
       color: "brand.100",
       _hover: {
