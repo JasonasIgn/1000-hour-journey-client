53ce3f88bb3c999b30867c01b1eab6828136bfc8
Ignas <ignas.jasonas@telesoftas.com>
2022-08-23 22:28:07 +0300

    Fix overflowing image height for journey list item

--- src/components/JourneysListItem/JourneysListItem.tsx
+++ src/components/JourneysListItem/JourneysListItem.tsx
@@ -55,7 +55,7 @@ export const JourneysListItem: FC<JourneysListItemProps> = ({
         alignItems="center"
         justifyContent="center"
       >
-        <Image src={journey.mediaUrl || Logo} />
+        <Image src={journey.mediaUrl || Logo} maxHeight="100%" />
       </Flex>
       <Box ml={5}>
         <Heading size="lg">{journey.title}</Heading>
