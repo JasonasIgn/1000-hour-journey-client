4674e724137f5c462278be1df5f0e2d1ef9ad82d
Ignas <ignas.jasonas@telesoftas.com>
2022-09-09 22:22:46 +0300

    Implement controlled tabs

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,22 +1,47 @@
 import { Flex, Image, Link, Tab, TabList, Tabs } from "@chakra-ui/react";
-import { Link as RouterLink } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { Link as RouterLink, useLocation } from "react-router-dom";
 import Logo from "resources/logo.png";
 
+interface RouteTab {
+  path: string;
+  name: string;
+}
+
+const tabs: RouteTab[] = [
+  { path: "/dashboard", name: "Dashboard" },
+  { path: "/journeys", name: "Journeys" },
+];
+
 export const Header = () => {
+  const location = useLocation();
+  const [tabIndex, setTabIndex] = useState(0);
+
+  const handleTabsChange = (index: number) => {
+    setTabIndex(index);
+  };
+
+  useEffect(() => {
+    setTabIndex(tabs.findIndex((tab) => location.pathname.includes(tab.path)));
+  }, [location]);
+
   return (
     <Flex height="60px" padding={2.5} alignItems="center">
       <Link as={RouterLink} to="/" height="100%">
         <Image src={Logo} alt="1000 hour journeys logo" height="100%" />
       </Link>
       <Flex ml={4}>
-        <Tabs variant="soft-rounded" colorScheme="green">
+        <Tabs
+          variant="soft-rounded"
+          index={tabIndex}
+          onChange={handleTabsChange}
+        >
           <TabList>
-            <Tab as={RouterLink} to="/dashboard">
-              Dashboard
-            </Tab>
-            <Tab as={RouterLink} to="/journeys">
-              Journeys
-            </Tab>
+            {tabs.map((tab) => (
+              <Tab as={RouterLink} to={tab.path} key={tab.name}>
+                {tab.name}
+              </Tab>
+            ))}
           </TabList>
         </Tabs>
       </Flex>
