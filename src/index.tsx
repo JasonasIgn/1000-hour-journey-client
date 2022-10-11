aeb3b637c3df40c1c32f67d4265961d0ae9e5e57
Ignas <ignas.jasonas@telesoftas.com>
2022-10-11 20:21:18 +0300

    Implement 401 interceptor

--- src/index.tsx
+++ src/index.tsx
@@ -6,9 +6,12 @@ import { ChakraProvider } from "@chakra-ui/react";
 import App from "./App";
 import "./index.css";
 import theme from "./theme";
+import { createAxiosInterceptor } from "utils/interceptors";
 
 const container = document.getElementById("root")!;
 const root = createRoot(container);
+
+createAxiosInterceptor(store);
 
 root.render(
   <Provider store={store}>
---
+++ src/utils/interceptors.ts
@@ -0,0 +1,20 @@
+import { Store } from "@reduxjs/toolkit";
+import axios, { AxiosResponse } from "axios";
+import { AppDispatch, RootState } from "store";
+import { logOut } from "store/features/auth/slice";
+
+const axios401ResponseInterceptor = (store: Store<RootState>) => [
+  (response: AxiosResponse) => response,
+  (error: any) => {
+    const dispatch = store.dispatch as AppDispatch;
+    if (error?.response?.status === 401) {
+      dispatch(logOut());
+    }
+    return Promise.reject(error);
+  },
+];
+
+export const createAxiosInterceptor = (store: Store<RootState>) => {
+    axios.interceptors.response.use(...axios401ResponseInterceptor(store));
+  };
+  
