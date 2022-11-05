2f7aebeb265a95ddc1af36267e60bdf8ff8cb420
Ignas <ignas.jasonas@telesoftas.com>
2022-11-05 21:43:54 +0200

    Implement log list filtering

--- src/components/JourneyItemsList/JourneyItemsList.tsx
+++ src/components/JourneyItemsList/JourneyItemsList.tsx
@@ -1,10 +1,16 @@
-import { FC, useEffect, useRef } from "react";
+import { FC, useEffect, useMemo, useRef, useState } from "react";
 import { Divider, Flex, Heading } from "@chakra-ui/react";
 import { Log, Tag } from "store/features/journeys/types";
 import { JourneyItemsListItem } from "./JourneyItemsListItem/JourneyItemsListItem";
-import { Select } from "chakra-react-select";
+import {
+  ChakraStylesConfig,
+  GroupBase,
+  MultiValue,
+  Select,
+} from "chakra-react-select";
 import { getTagOptions } from "components/JourneyLogDialogs/utils";
 import { chakraStyles } from "components/CreatableSelectField/style";
+import { Option } from "types";
 
 interface JourneyItemsListProps {
   logs: Log[];
@@ -20,10 +26,20 @@ export const JourneyItemsList: FC<JourneyItemsListProps> = ({
   tags,
 }) => {
   const activeRef = useRef<HTMLDivElement>(null);
+  const [filterTags, setFilterTags] = useState<MultiValue<Option>>([]);
 
   useEffect(() => {
     activeRef.current?.scrollIntoView({ behavior: "smooth", block: "center" });
   }, [activeLog]);
+
+  const filteredLogs = useMemo(() => {
+    return logs.filter((log) =>
+      filterTags.every(
+        (filterTag) =>
+          log.tags.findIndex((tag) => tag.id === filterTag.value) >= 0
+      )
+    );
+  }, [logs, filterTags]);
 
   return (
     <Flex direction="column" w="full" mb={2}>
@@ -35,16 +51,21 @@ export const JourneyItemsList: FC<JourneyItemsListProps> = ({
         <Divider ml={2} borderColor="brand.100" />
       </Flex>
       <Flex direction="column" px={2} mb={3}>
-        <Select
+        <Select<Option, true, GroupBase<Option>>
           isMulti
           name="colors"
           options={getTagOptions(tags)}
           placeholder="Filter by tag..."
-          chakraStyles={chakraStyles}
+          chakraStyles={
+            chakraStyles as ChakraStylesConfig<Option, true, GroupBase<Option>>
+          }
+          onChange={(val) => {
+            setFilterTags(val);
+          }}
         />
       </Flex>
       <Flex direction="column" overflow="auto" px={1}>
-        {logs.map((log, idx) => (
+        {filteredLogs.map((log, idx) => (
           <JourneyItemsListItem
             log={log}
             index={idx + 1}
