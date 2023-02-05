e0c08e2b3695336dbdee0083d7fb44d0b78ee006
Ignas <ignas.jasonas@telesoftas.com>
2023-02-05 20:11:09 +0200

    Use global image viewer everywhere

--- src/components/ShowcaseCard/AchievementShowcaseCardContent.tsx
+++ src/components/ShowcaseCard/AchievementShowcaseCardContent.tsx
@@ -1,10 +1,8 @@
-import { Box, Flex, FlexProps, Heading, Image, Text } from "@chakra-ui/react";
-import { FC, useState } from "react";
+import { FC } from "react";
+import { Box, Flex, FlexProps, Heading, Text } from "@chakra-ui/react";
 import format from "date-fns/format";
-import ImageViewer from "react-simple-image-viewer";
 import { Achievement } from "store/features/journeys/types";
 import { dateFormats } from "utils/constants";
-import { getImageSrc } from "utils/helpers";
 
 interface AchievementShowcaseCardContentProps extends FlexProps {
   achievement: Achievement;
@@ -13,7 +11,6 @@ interface AchievementShowcaseCardContentProps extends FlexProps {
 export const AchievementShowcaseCardContent: FC<
   AchievementShowcaseCardContentProps
 > = ({ achievement, ...rest }) => {
-  const [currentViewedImage, setCurrentViewedImage] = useState("");
   return (
     <>
       <Flex
@@ -45,38 +42,7 @@ export const AchievementShowcaseCardContent: FC<
             {achievement.description}
           </Text>
         </Flex>
-        {achievement?.mediaUrl && (
-          <Flex height="100%" width="60%" justifyContent="center" pl={3} ml={3}>
-            <Image
-              cursor="pointer"
-              src={`${getImageSrc(
-                achievement.mediaUrl
-              )}?${achievement.updatedAt.toString()}`} // prevent caching
-              alt={`${achievement.id} media`}
-              margin="auto"
-              maxHeight="100%"
-              boxShadow="0px 0px 15px var(--chakra-colors-brand-100)"
-              borderRadius={12}
-              onClick={() => {
-                setCurrentViewedImage(
-                  `${getImageSrc(
-                    achievement.mediaUrl
-                  )}?${achievement.updatedAt.toString()}`
-                );
-              }}
-            />
-          </Flex>
-        )}
       </Flex>
-      {Boolean(currentViewedImage) && (
-        <ImageViewer
-          src={[currentViewedImage]}
-          currentIndex={0}
-          disableScroll={false}
-          closeOnClickOutside={true}
-          onClose={() => setCurrentViewedImage("")}
-        />
-      )}
     </>
   );
 };
