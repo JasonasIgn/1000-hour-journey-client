87faeac8ea756e73ec74c05069005116bd9b7876
Ignas <ignas.jasonas@telesoftas.com>
2023-02-05 16:13:04 +0200

    Move edit button to the list

--- src/components/JourneyItemsList/JourneyItemsListItem/JourneyItemsListItem.tsx
+++ src/components/JourneyItemsList/JourneyItemsListItem/JourneyItemsListItem.tsx
@@ -1,14 +1,21 @@
+import { EditIcon } from "@chakra-ui/icons";
 import { Flex, FlexProps, forwardRef, Heading, Text } from "@chakra-ui/react";
+import { AppDispatch } from "store";
+import { setEditLogDialogOpen } from "store/features/journey/slice";
 import { Log } from "store/features/journeys/types";
 
 interface JourneyItemsListItemProps extends FlexProps {
   log: Log;
   index: number;
   active: boolean;
+  dispatch: AppDispatch;
 }
 
 export const JourneyItemsListItem = forwardRef(
-  ({ log, index, active, ...rest }: JourneyItemsListItemProps, ref) => (
+  (
+    { log, index, active, dispatch, ...rest }: JourneyItemsListItemProps,
+    ref
+  ) => (
     <Flex
       w="full"
       border="1px solid"
@@ -31,12 +38,23 @@ export const JourneyItemsListItem = forwardRef(
       <Heading size="sm">#{index}</Heading>
       <Text
         ml={4}
+        pr={4}
         textOverflow="ellipsis"
         overflow="hidden"
         whiteSpace="nowrap"
       >
         {log.description}
       </Text>
+      {active && (
+        <EditIcon
+          width="22px"
+          fill="gray.300"
+          ml="auto"
+          onClick={() => {
+            dispatch(setEditLogDialogOpen(true));
+          }}
+        />
+      )}
     </Flex>
   )
 );
--- src/components/ShowcaseCard/LogShowcaseCardContent.tsx
+++ src/components/ShowcaseCard/LogShowcaseCardContent.tsx
@@ -1,10 +1,8 @@
 import {
   Box,
-  chakra,
   Flex,
   FlexProps,
   Heading,
-  IconButton,
   Image,
   Tag,
   TagLabel,
@@ -13,48 +11,22 @@ import {
 import { FC, useState } from "react";
 import format from "date-fns/format";
 import ImageViewer from "react-simple-image-viewer";
-import { ReactComponent as EditIconComponent } from "resources/edit.svg";
 import { LogExtended } from "store/features/journeys/types";
 import { dateFormats } from "utils/constants";
-import { useAppDispatch } from "store/hooks";
-import { setEditLogDialogOpen } from "store/features/journey/slice";
 import { getImageSrc } from "utils/helpers";
 
 interface LogShowcaseCardContentProps extends FlexProps {
   log: LogExtended;
 }
 
-const EditIcon = chakra(EditIconComponent);
-
 export const LogShowcaseCardContent: FC<LogShowcaseCardContentProps> = ({
   log,
   ...rest
 }) => {
-  const dispatch = useAppDispatch();
   const [currentViewedImage, setCurrentViewedImage] = useState("");
 
   return (
     <>
-      <Flex
-        position="absolute"
-        top={0}
-        right={0}
-        height="100%"
-        alignItems="center"
-      >
-        <IconButton
-          icon={<EditIcon width="22px" fill="gray.300" />}
-          aria-label="edit"
-          position="absolute"
-          borderRadius="50%"
-          right="-20px"
-          color="brand.700"
-          border="1px solid"
-          onClick={() => {
-            dispatch(setEditLogDialogOpen(true));
-          }}
-        />
-      </Flex>
       <Flex
         justifyContent="space-between"
         borderBottom="1px solid"
