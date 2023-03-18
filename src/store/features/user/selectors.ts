c4aa167a2be81403ae530bb1b5d6b04fe8865edb
Ignas <ignas.jasonas@telesoftas.com>
2023-03-18 16:58:58 +0200

    Add reward for logged time display

--- src/store/features/user/selectors.ts
+++ src/store/features/user/selectors.ts
@@ -1,3 +1,5 @@
 import { RootState } from "store/reducers";
 
 export const getUserPoints = (state: RootState) => state.user.points;
+
+export const getUserReward = (state: RootState) => state.user.reward;
---
+++ src/theme/components/Popover.ts
@@ -0,0 +1,12 @@
+import { ComponentStyleConfig } from "@chakra-ui/react";
+
+export const Popover: ComponentStyleConfig = {
+  baseStyle: {},
+  sizes: {
+    sm: {
+      content: {
+        width: "200px",
+      },
+    },
+  },
+};
