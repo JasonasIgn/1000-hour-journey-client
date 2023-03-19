cff752f692bf8f3458761fe2900d1d8bff618598
Ignas <ignas.jasonas@telesoftas.com>
2023-03-19 21:21:54 +0200

    add basic list & empty state

---
+++ src/components/ShopItemsList/ShopItemsList.tsx
@@ -0,0 +1,36 @@
+import { FC, Dispatch, SetStateAction } from "react";
+import { Box, Center, Heading } from "@chakra-ui/react";
+import { Loader } from "components";
+import { ShopItem } from "store/features/shop/types";
+
+interface ShopItemsListProps {
+  items: ShopItem[];
+  openEditItemDialog: Dispatch<SetStateAction<ShopItem | null>>;
+  isLoading: boolean;
+}
+
+export const ShopItemsList: FC<ShopItemsListProps> = ({
+  items = [],
+  openEditItemDialog,
+  isLoading,
+}) => {
+  if (isLoading) {
+    return <Loader />;
+  }
+
+  if (items.length === 0) {
+    return (
+      <Center flexGrow={1} mb="12vh">
+        <Heading>No items yet...</Heading>
+      </Center>
+    );
+  }
+
+  return (
+    <Box mt={5}>
+      {items.map((item) => (
+        <Box> item</Box>
+      ))}
+    </Box>
+  );
+};
--- src/views/ShopView/ShopViewContent.tsx
+++ src/views/ShopView/ShopViewContent.tsx
@@ -1,15 +1,31 @@
-import { FC, useState } from "react";
+import { FC, useEffect, useState } from "react";
 import { Button, Container, Flex } from "@chakra-ui/react";
 import { AddIcon } from "@chakra-ui/icons";
 import { Paper } from "components/Paper";
 import { ShopItem } from "store/features/shop/types";
+import { ShopItemsList } from "components/ShopItemsList";
+import { useAppDispatch, useAppSelector } from "store/hooks";
+import {
+  getShopItemsList,
+  getShopItemsListLoadingState,
+} from "store/features/shop/selectors";
+import { fetchShopItemsListEffect } from "store/features/shop/effects";
 
 export const ShopViewContent: FC = () => {
+  const dispatch = useAppDispatch();
+  const listLoadingState = useAppSelector(getShopItemsListLoadingState);
+  const shopItemsList = useAppSelector(getShopItemsList);
   const [addModalOpen, setAddModalOpen] = useState(false);
   const [editingItem, setEditingItem] = useState<ShopItem | null>(null);
 
+  useEffect(() => {
+    if (listLoadingState === "pristine") {
+      dispatch(fetchShopItemsListEffect());
+    }
+  }, [dispatch, listLoadingState]);
+
   return (
-    <Container maxW="8xl" pt={5}>
+    <Container maxW="6xl" py={5} flexGrow={1}>
       <Paper flexDirection="column" height="100%" sx={{ borderRadius: 0 }}>
         <Flex borderBottom="1px solid" borderColor="brand.600" py={5} px={5}>
           <Button
@@ -21,7 +37,11 @@ export const ShopViewContent: FC = () => {
           </Button>
         </Flex>
         <Flex direction="column" px={5} overflow="auto" flexGrow={1}>
-          list here
+          <ShopItemsList
+            isLoading={["pristine", "loading"].includes(listLoadingState)}
+            items={shopItemsList}
+            openEditItemDialog={setEditingItem}
+          />
         </Flex>
       </Paper>
     </Container>
